b0fe5ec1e916b9c9165e0dba7c7d84ba1cd695ac
cafeeb0m <142467179+cafeeb0m@users.noreply.github.com>
2024-07-30 21:33:40 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,css,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#A767FF",
+      }
+    },
+  },
+  plugins: [],
+}
+
